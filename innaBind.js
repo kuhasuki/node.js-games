c7a7a690f5fce7dab7d77eb2d44c919ca1302d2f
App Academy Student <student@appacademy.io>
2015-11-27 18:05:06 -0800

    End of day friday, run almost complete

--- innaBind.js
+++ innaBind.js
@@ -1,7 +1,32 @@
 
-Function.prototype.myBind(context) {
+Function.prototype.myBind = function(context) {
   var fn = this;
-  function() {
+  var ff = function() {
     fn.apply(context);
-  }
+  };
+  return ff;
+};
+
+
+
+function Clock () {
+
+	this.date = new Date();
+	this.hours = this.date.getHours();
+	this.minutes = this.date.getMinutes();
+	this.seconds = this.date.getSeconds();
+
+	setInterval(this._tick.myBind(this), 1000);
+
 }
+
+Clock.prototype.printTime = function () {
+  console.log(this.hours + ":" + this.minutes + ":" + this.seconds);
+};
+
+Clock.prototype._tick = function () {
+  this.seconds += 1;
+  this.printTime();
+};
+
+var c = new Clock;
--- towersOfHanoi.js
+++ towersOfHanoi.js
@@ -1,23 +1,76 @@
-function HanoiGame () {
-	stacks: [[3,2,1],[],[]],
-	isWon: function (){
-		if (this.stacks === [[],[],[3,2,1]] || this.stacks === [[],[3,2,1],[]])
-			return true;
-		else 
-			return false;
-	},
-	isValidMove: function (startIdx, endIdx) {
-		if (stacks[startIdx].length === 0)
-			return false;
-		else if (stacks[startIdx][startIdx.length - 1] > stacks[endIdx][endIdx.length - 1]) 
-			return false;
-		else if (stacks[startIdx] === undefined || stacks[endIdx] === undefined)
-			return false;
-		else
-			return true;
-	}
+var readline = require("readline");
 
+var reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function HanoiGame () {
+	this.stacks = [[3,2,1],[],[]];
 }
 
-testGame = new HanoiGame;
-console.log(testGame.stacks);
+HanoiGame.prototype.isValidMove = function (startIdx, endIdx) {
+	console.log(this.stacks);
+	if (typeof this.stacks[startIdx] === "undefined" || typeof this.stacks[endIdx] === "undefined")
+		return false;
+	else if (this.stacks[startIdx][startIdx.length - 1] > this.stacks[endIdx][endIdx.length - 1])
+		return false;
+	else if (this.stacks[startIdx].length === 0)
+		return false;
+	else
+		return true;
+};
+
+HanoiGame.prototype.isWon = function(){
+	if ((this.stacks[0].length === 0 && this.stacks[1].length === 0) ||  (this.stacks[0].length === 0 && this.stacks[2].length === 0))
+		return true;
+	else
+		return false;
+};
+
+HanoiGame.prototype.move = function(startTowerIdx, endTowerIdx){
+	this.stacks[endTowerIdx].push(this.stacks[startTowerIdx].pop());
+	console.log(this.stacks);
+};
+
+HanoiGame.prototype.print = function() {
+	console.log(JSON.stringify(this.stacks));
+};
+
+HanoiGame.prototype.promptMove = function(callback) {
+	var gamePrompt = function(response) {
+		var moves = response.split(", ");
+		if (this.isValidMove(moves[0], moves[1])){
+			this.move(moves[0], moves[1]);
+			callback();
+		}
+		else
+			this.promptMove();
+	};
+
+	reader.question("Give start and end (you know what to do)!\n", gamePrompt.bind(this));
+
+};
+
+HanoiGame.prototype.run = function(completionCallback) {
+	var killbock = function(){
+		if (!this.isWon()) {
+			this.promptMove(killbock);
+		}
+		else {
+			return;
+		}
+	};
+
+	if(this.isWon())
+		completionCallback();
+	else {
+		this.promptMove(killbock.bind(this));
+	}
+};
+
+
+var game = new HanoiGame;
+game.run(function() {
+	console.log("You won!");
+});
